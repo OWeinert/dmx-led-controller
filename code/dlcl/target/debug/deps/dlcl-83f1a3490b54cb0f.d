90e6b6d22a207ba599c0e6b8eb01daff3d968149
olew <mr.piggo123@gmail.com>
2023-08-11 17:36:31 +0200

    Changed .gitignore files

--- code/dlcl/target/debug/deps/dlcl-83f1a3490b54cb0f.d
+++ code/dlcl/target/debug/deps/dlcl-83f1a3490b54cb0f.d
@@ -1,6 +1,6 @@
-/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/deps/dlcl-83f1a3490b54cb0f.rmeta: src/lib.rs src/interop/mod.rs src/interop/c/mod.rs src/interop/python/mod.rs src/interop/python/core.rs src/interop/python/ext_module.rs src/draw/mod.rs
+/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/deps/dlcl-83f1a3490b54cb0f.rmeta: src/lib.rs src/interop/mod.rs src/interop/c/mod.rs src/interop/python/mod.rs src/interop/python/core.rs src/interop/python/ext_module.rs
 
-/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/deps/dlcl-83f1a3490b54cb0f.d: src/lib.rs src/interop/mod.rs src/interop/c/mod.rs src/interop/python/mod.rs src/interop/python/core.rs src/interop/python/ext_module.rs src/draw/mod.rs
+/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/deps/dlcl-83f1a3490b54cb0f.d: src/lib.rs src/interop/mod.rs src/interop/c/mod.rs src/interop/python/mod.rs src/interop/python/core.rs src/interop/python/ext_module.rs
 
 src/lib.rs:
 src/interop/mod.rs:
@@ -8,4 +8,3 @@ src/interop/c/mod.rs:
 src/interop/python/mod.rs:
 src/interop/python/core.rs:
 src/interop/python/ext_module.rs:
-src/draw/mod.rs:
