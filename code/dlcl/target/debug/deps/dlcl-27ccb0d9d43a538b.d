0e49e73613e377ad1fced193600f51ee8804011d
olew <mr.piggo123@gmail.com>
2023-08-11 13:08:13 +0200

    Update dlcl targets

--- code/dlcl/target/debug/deps/dlcl-27ccb0d9d43a538b.d
+++ code/dlcl/target/debug/deps/dlcl-27ccb0d9d43a538b.d
@@ -1,10 +1,10 @@
-/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/deps/dlcl-27ccb0d9d43a538b.rmeta: src/lib.rs src/interop/mod.rs src/interop/c/mod.rs src/interop/python/mod.rs src/interop/python/io.rs src/draw/mod.rs
+/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/deps/dlcl-27ccb0d9d43a538b.rmeta: src/lib.rs src/interop/mod.rs src/interop/c/mod.rs src/interop/python/mod.rs src/interop/python/core.rs src/draw/mod.rs
 
-/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/deps/dlcl-27ccb0d9d43a538b.d: src/lib.rs src/interop/mod.rs src/interop/c/mod.rs src/interop/python/mod.rs src/interop/python/io.rs src/draw/mod.rs
+/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/deps/dlcl-27ccb0d9d43a538b.d: src/lib.rs src/interop/mod.rs src/interop/c/mod.rs src/interop/python/mod.rs src/interop/python/core.rs src/draw/mod.rs
 
 src/lib.rs:
 src/interop/mod.rs:
 src/interop/c/mod.rs:
 src/interop/python/mod.rs:
-src/interop/python/io.rs:
+src/interop/python/core.rs:
 src/draw/mod.rs:
