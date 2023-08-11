f0790827d8f39aeb6028f1b08ad450a64f6d9c17
olew <mr.piggo123@gmail.com>
2023-08-11 17:16:28 +0200

    Add module ext_module

--- code/dlcl/target/debug/deps/dlcl-9ccf1fd680b5526f.d
+++ code/dlcl/target/debug/deps/dlcl-9ccf1fd680b5526f.d
@@ -1,10 +1,11 @@
-/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/deps/dlcl-9ccf1fd680b5526f.rmeta: src/lib.rs src/interop/mod.rs src/interop/c/mod.rs src/interop/python/mod.rs src/interop/python/core.rs src/draw/mod.rs
+/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/deps/dlcl-9ccf1fd680b5526f.rmeta: src/lib.rs src/interop/mod.rs src/interop/c/mod.rs src/interop/python/mod.rs src/interop/python/core.rs src/interop/python/ext_module.rs src/draw/mod.rs
 
-/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/deps/dlcl-9ccf1fd680b5526f.d: src/lib.rs src/interop/mod.rs src/interop/c/mod.rs src/interop/python/mod.rs src/interop/python/core.rs src/draw/mod.rs
+/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/deps/dlcl-9ccf1fd680b5526f.d: src/lib.rs src/interop/mod.rs src/interop/c/mod.rs src/interop/python/mod.rs src/interop/python/core.rs src/interop/python/ext_module.rs src/draw/mod.rs
 
 src/lib.rs:
 src/interop/mod.rs:
 src/interop/c/mod.rs:
 src/interop/python/mod.rs:
 src/interop/python/core.rs:
+src/interop/python/ext_module.rs:
 src/draw/mod.rs:
