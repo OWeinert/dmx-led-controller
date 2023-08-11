51f75ba90f3ca458795b6364d9c177ee9dbc2bc8
olew <mr.piggo123@gmail.com>
2023-08-11 17:13:32 +0200

    Add pyo3 features

---
+++ code/dlcl/target/debug/build/pyo3-fcaaeef6223645e3/build_script_build-fcaaeef6223645e3.d
@@ -0,0 +1,5 @@
+/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/build/pyo3-fcaaeef6223645e3/build_script_build-fcaaeef6223645e3: /home/ole/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-0.19.2/build.rs
+
+/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/build/pyo3-fcaaeef6223645e3/build_script_build-fcaaeef6223645e3.d: /home/ole/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-0.19.2/build.rs
+
+/home/ole/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-0.19.2/build.rs:
---
+++ code/dlcl/target/debug/deps/dlcl-83f1a3490b54cb0f.d
@@ -0,0 +1,10 @@
+/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/deps/dlcl-83f1a3490b54cb0f.rmeta: src/lib.rs src/interop/mod.rs src/interop/c/mod.rs src/interop/python/mod.rs src/interop/python/core.rs src/draw/mod.rs
+
+/home/ole/dev/coding-project/dmx-led-controller/code/dlcl/target/debug/deps/dlcl-83f1a3490b54cb0f.d: src/lib.rs src/interop/mod.rs src/interop/c/mod.rs src/interop/python/mod.rs src/interop/python/core.rs src/draw/mod.rs
+
+src/lib.rs:
+src/interop/mod.rs:
+src/interop/c/mod.rs:
+src/interop/python/mod.rs:
+src/interop/python/core.rs:
+src/draw/mod.rs:
