2acc95d911e86901023df4f19af8b643227e4ea5
olew <mr.piggo123@gmail.com>
2023-08-12 01:52:06 +0200

    Implement functionality to exisiting functions

--- code/dlcl/src/interop/python/ext_module.rs
+++ code/dlcl/src/interop/python/ext_module.rs
@@ -1,4 +1,7 @@
+use embedded_graphics::{pixelcolor::Rgb888, prelude::Point};
 use pyo3::prelude::*;
+
+use crate::draw;
 
 #[pyclass]
 struct PyRgb888 {
@@ -7,14 +10,22 @@ struct PyRgb888 {
     b: u8
 }
 
-#[pyfunction]
-fn draw_pixel(x: usize, y: usize, color: &PyRgb888) {
-    
+impl PyRgb888 {
+    fn to_rgb888(&self) -> Rgb888 {
+        Rgb888::new(self.r, self.g, self.b)
+    }
 }
 
+/// Python binding for *draw::draw_pixel*
+#[pyfunction]
+fn draw_pixel(x: i32, y: i32, color: &PyRgb888) {
+    let rs_col = color.to_rgb888();
+    draw::draw_pixel(Point::new(x, y), rs_col);
+}
+
+/// The Python module configuration for dlcl
 #[pymodule]
 fn dlcl(py: Python<'_>, m: &PyModule) -> PyResult<()> {
-
-
+    m.add_function(wrap_pyfunction!(draw_pixel, m)?)?;
     Ok(())
 }
