5c7fa552c6aaa7f184d45bfa4a845e1e58a91f63
Ole <mr.piggo123@gmail.com>
2023-10-05 22:08:15 +0200

    Added Dto mapping extension methods

---
+++ code/DlclNet/DlclNet/Extensions/DTOExtensions.cs
@@ -0,0 +1,47 @@
+using DlclNet.Models;
+using DlclRpc;
+
+namespace DlclNet.Extensions;
+
+public static class DTOExtensions
+{
+    public static Animation ToAnimation(this AnimationDTO dto)
+    {
+        var frames = dto.Frames.ToList().ConvertAll(f => f.ToFrame());
+        var anim = new Animation
+        {
+            Layer = dto.Layer,
+            Frames = frames
+        };
+        return anim;
+    }
+
+    public static Frame ToFrame(this FrameDTO dto)
+    {
+        var pixels = dto.Pixels.ToList().ConvertAll(p => p.ToPixel());
+        var frame = new Frame
+        {
+            Pixels = pixels
+        };
+        return frame;
+    }
+
+    public static Pixel ToPixel(this PixelDTO dto)
+    {
+        var pixel = new Pixel
+        {
+            Color = new Color
+            {
+                R = dto.R,
+                G = dto.G,
+                B = dto.B
+            },
+            Position = new Position
+            {
+                X = dto.X,
+                Y = dto.Y
+            }
+        };
+        return pixel;
+    }
+}
